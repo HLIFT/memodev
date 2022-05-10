6d8d7dfe793d2d599adb3cd966c280baac934f61
guillaume-ventura <venturaguillaume01@gmail.com>
2022-05-10 10:53:31 +0200

    ✨ Add algo

--- helpers/devs.ts
+++ helpers/devs.ts
@@ -1,47 +1,58 @@
 export const listDevs: Dev[] = [
     {
+        id: 1,
         name: "Elias",
         image: require("../assets/images/img_elias.png")
     },
     {
+        id: 2,
         name: "Léo",
         image: require("../assets/images/img_leo.png")
     },
     {
+        id: 3,
         name: "Alexis",
         image: require("../assets/images/img_alexis.png")
     },
     {
+        id: 4,
         name: "Samuel",
         image: require("../assets/images/img_samuel.png")
     },
     {
+        id: 5,
         name: "Axel",
         image: require("../assets/images/img_axel.png")
     },
     {
+        id: 6,
         name: "Guillaume",
         image: require("../assets/images/img_guillaume.png")
     },
     {
+        id: 7,
         name: "Jordan",
         image: require("../assets/images/img_jordan.png")
     },
     {
+        id: 8,
         name: "Paul",
         image: require("../assets/images/img_paul.png")
     },
     {
+        id: 9,
         name: "Monica",
         image: require("../assets/images/img_monica.png")
     },
     {
+        id: 10,
         name: "Florentin",
         image: require("../assets/images/img_florentin.png")
     },
 ]
 
 export type Dev = {
+    id: number,
     name: string,
     image: string
 }
--- screens/GameScreen.tsx
+++ screens/GameScreen.tsx
@@ -1,4 +1,4 @@
-import {StyleSheet, View} from "react-native";
+import {StyleSheet, TouchableOpacity, View} from "react-native";
 import {screenStyles} from "../assets/styles/screen.style";
 import Card from "../components/Card";
 import {useEffect, useState} from "react";
@@ -6,7 +6,15 @@ import {Dev, listDevs} from "../helpers/devs";
 
 const GameScreen = () => {
 
+    interface DevReturned {
+        dev: Dev,
+        index: number
+    }
+
     const [devs, setDevs] = useState<Dev[]>([])
+    const [devsReturned, setDevsReturned] = useState<DevReturned[]>([])
+    const [lastReturned, setLastReturned] = useState<DevReturned|undefined>(undefined)
+    const [clickable, setClickable] = useState<boolean>(true)
 
     useEffect(() => {
         setDevs(shuffleDevs(listDevs))
@@ -28,11 +36,45 @@ const GameScreen = () => {
         return finalArray
     }
 
+    const handleClickOnCard = (dev: Dev, index: number) => {
+        setClickable(false)
+        returnCard(dev, index)
+    }
+
+    const returnCard = (dev: Dev, index: number) => {
+        setDevsReturned([...devsReturned, {dev, index}])
+        window.setTimeout(() => {
+            checkIfGood(dev, index)
+        }, 1500)
+    }
+
+    const checkIfGood = (dev: Dev, index: number) => {
+        if (lastReturned !== undefined) {
+            if(lastReturned.dev.id === dev.id) {
+                setDevsReturned([...devsReturned, {dev, index}])
+                setLastReturned(undefined)
+            } else {
+                setDevsReturned(devsReturned.filter(value => value.index !== lastReturned.index && value.index !== index))
+                setLastReturned(undefined)
+            }
+        } else {
+            setDevsReturned([...devsReturned, {dev, index}])
+            setLastReturned({dev, index})
+        }
+        setClickable(true)
+    }
+
     return (
         <View style={[styles.board]}>
             {devs.map((dev, index) => {
                 return (
-                    <Card key={dev.name+'-'+index} image={dev.image} />
+                    <TouchableOpacity key={dev.id+'-'+index} onPress={clickable ? () => handleClickOnCard(dev, index) : () => {}}>
+                        {devsReturned.find(value => value.index === index) ?
+                            <Card image={dev.image} />
+                            :
+                            <Card image={require("../assets/images/img_anonyme.png")} />
+                        }
+                    </TouchableOpacity>
                 )
             })}
         </View>
